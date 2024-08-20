e8f1c2e0f4c1a094cd2b70876e45803ba1ea238a
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-08-20 12:08:48 +0530

    Time: 49 ms (36.78%) | Memory: 13 MB (67.16%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // Nested loop to check all pairs
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j}; 
+                }
+            }
+        }
+        return {}; 
+    }
+};
