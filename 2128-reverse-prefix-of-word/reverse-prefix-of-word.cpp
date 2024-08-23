d8d7a904b85e0cf040e156424ae48f011b1b74d5
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-08-23 06:02:20 +0530

    Time: 3 ms (42.14%) | Memory: 7.6 MB (64.44%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -3,12 +3,12 @@ public:
     string reversePrefix(string word, char ch) {
        
         int index = word.find(ch);
-        if (index != std::string::npos) {
+        if (index != string::npos) {
             
             int left = 0;
             int right = index;
             while (left < right) {
-                std::swap(word[left], word[right]);
+                swap(word[left], word[right]);
                 left++;
                 right--;
             }
