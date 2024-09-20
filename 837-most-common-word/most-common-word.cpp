634dfc50bfba1699a649abe936caf31edcef567f
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-09-20 14:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (60.40%) - LeetSync

---
+++ 837-most-common-word/most-common-word.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+
+        for(auto &c : paragraph){
+            if(!isalpha(c)) c = ' ';
+            else c = tolower(c);
+        }
+
+        unordered_map<string, int> map;
+        int maxf = 0;
+        string res;
+
+        stringstream ss(paragraph);
+        string str;
+        while(ss >> str){
+            if(find(banned.begin(), banned.end(), str) == banned.end()){
+                map[str]++;
+                if(map[str] >= maxf){
+                    maxf = map[str];
+                    res = str;
+                } 
+            }
+        }
+
+        return res;
+    }
+};
