3000698c12a2d055cc1037f4b32e921e1a605d43
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-09-28 15:08:07 +0530

    Time: 6 ms (44.97%) | Memory: 8.5 MB (70.56%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i=1,c=0;
-       while(n>=i)
-       {
-         n-=i;
-         c++;
-         i++;
-       }
-       return c;
+        int l=0,r=n;
+        while(l<=r){
+           long long mid=(l+r)/2;
+           long long coins=(mid*(mid+1))/2;
+            if(coins==n){
+                return mid;
+            }else if(coins<n){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return r;
     }
 };
