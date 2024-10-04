cd3689583c2bc56e93ab18bb93c68f7d65e956ec
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-10-04 15:09:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (51.45%) - LeetSync

---
+++ 755-reach-a-number/reach-a-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target=abs(target);
+        int sum=0,step=0;
+        while(sum<target || (sum-target)%2!=0)
+        {
+            step++;
+            sum+=step;
+        }
+        return step;
+    }
+};
