72355f4235f3d0b06e93e43256663835b6f6f519
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-10-06 12:51:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (77.16%) - LeetSync

--- 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.cpp
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.cpp
@@ -1,13 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     int divisorSubstrings(int num, int k) {
         string s = to_string(num);
         int count = 0;
-
-        for (int i = 0; i <= s.size()-k; i++) {
-            string l = s.substr(i, k);
-            int sub_num = stoi(l);
-
+        for (int start = 0; start <= s.size() - k; start++) {
+            int sub_num = 0;
+            for (int end = start; end < start + k; end++) {
+                sub_num = sub_num * 10 + (s[end] - '0'); 
+            }
             if (sub_num != 0 && num % sub_num == 0) {
                 count++;
             }
@@ -16,3 +20,5 @@ public:
         return count;
     }
 };
+
+
