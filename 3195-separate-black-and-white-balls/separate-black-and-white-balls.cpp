60cdc0bb5beb9e225d14887354b729df2e85e0d6
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-10-15 14:37:59 +0530

    Time: 33 ms (53.47%) | Memory: 15.7 MB (45.68%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    long long minimumSteps(string& s) {
-        long long ans=0,li=0;
+    long long minimumSteps(string s) {
+        long long ans=0,c=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
-                ans+=(i-li);
-                li++;
-            }
+            if(s[i]=='1') c++;
+            else ans+=c;
         }
         return ans;
     }
