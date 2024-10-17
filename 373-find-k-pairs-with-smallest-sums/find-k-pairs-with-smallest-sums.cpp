e1a4c5ae5665d88972083c2f2aadfa503cd70e21
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-10-17 21:52:39 +0530

    Time: 112 ms (99.74%) | Memory: 135.1 MB (66.99%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<vector<int>> res;
-        if (nums1.empty() || nums2.empty() || k == 0) return res;
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> minHeap;
-        for (int i = 0; i < nums1.size() && i < k; i++) {
-            minHeap.push({nums1[i] + nums2[0], i, 0});
-        }
-        while (!minHeap.empty() && res.size() < k) {
-            auto [sum, i, j] = minHeap.top();
-            minHeap.pop();
-            res.push_back({nums1[i], nums2[j]});
-            if (j + 1 < nums2.size()) {
-                minHeap.push({nums1[i] + nums2[j + 1], i, j + 1});
-            }
-        }
+       vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
 
-        return res;
+    vector<vector<int>> res;
+   
+    priority_queue<pair<int,pair<int,int>>> pq;
+    
+    for(auto x:nums1){
+        for(auto y:nums2){
+            int sum=x+y;
+            if( pq.size()<k){
+                 pq.push({sum,{x,y}});
+            }
+            else if(sum < pq.top().first)
+            {
+                pq.pop();
+                pq.push({sum,{x,y}});
+            }
+            else
+                break;
+        }
     }
+    
+    while(!pq.empty() ){
+       auto p=pq.top().second;
+        pq.pop();
+        
+        res.push_back({p.first,p.second});
+    }
+    return res;
+}
 };
