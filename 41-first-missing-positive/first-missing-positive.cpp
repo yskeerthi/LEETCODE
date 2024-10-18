c88f83bc00f4ecfbcfd4f307b6fc87bdfa830cf1
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-10-18 06:24:18 +0530

    Time: 143 ms (11.76%) | Memory: 79.8 MB (7.86%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,28 +1,15 @@
-
 class Solution {
 public:
-    int firstMissingPositive(std::vector<int>& nums) {
-        int n = nums.size();
-        
-        // Rearrange the elements to their correct positions
-        for (int i = 0; i < n; ++i) {
-            // Keep swapping until the current number is in its correct position
-            // or it's out of the range [1, n]
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                std::swap(nums[i], nums[nums[i] - 1]);
-            }
+    int firstMissingPositive(vector<int>& nums) {
+        map<int,int> map;
+        for(int num:nums){
+            map[num]++;
         }
-        
-        // After rearranging, the first index where the value is not (i + 1)
-        // is the missing positive number
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != i + 1) {
-                return i + 1;
-            }
+        int i=1;
+        while(i){
+            if(map.find(i)!=map.end()) i++;
+            else break;
         }
-        
-        // If all numbers from 1 to n are present, return n + 1
-        return n + 1;
+        return i;
     }
 };
-
