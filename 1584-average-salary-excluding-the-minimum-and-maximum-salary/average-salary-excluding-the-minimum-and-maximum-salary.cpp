27f0bfef9d9584b4821ceb17ae0755c0427bdebf
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-12-02 17:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.05%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int maxi=*max_element(salary.begin(),salary.end());
-        int mini=*min_element(salary.begin(),salary.end());
-        double sum=0;
-        int count=0;
-        for(int i=0;i<salary.size();i++)
-        {
-            if(salary[i]!=maxi && salary[i]!=mini)
-            {
-                
-                sum+=salary[i];
-               count++;
-                
-            }
-        }
-        return sum/count;
+      sort(salary.begin(),salary.end());
+      double sum=0;
+      int c=0;
+      for(int i=1;i<salary.size()-1;i++)
+      {
+        sum+=salary[i];
+        c++;
+      }
+      return sum/c;
     }
 };
