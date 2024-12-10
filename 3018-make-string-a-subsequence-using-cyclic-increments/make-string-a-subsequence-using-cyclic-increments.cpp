36d7c32f67dce1d5e756bbb0c0e4d5bc8231b7c4
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-12-10 10:05:36 +0530

    Time: 5 ms (47.68%) | Memory: 17 MB (65.50%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -3,12 +3,11 @@ public:
   bool canMakeSubsequence(string str1, string str2) {
     int n = str1.length(), m = str2.length();
     int j = 0;
-
     for (int i = 0; i < n && j < m; i++) {
-        char currentChar = str1[i];
-        char incrementedChar = (currentChar - 'a' + 1) % 26 + 'a';
+        char currchar = str1[i];
+        char inchar = (currchar - 'a' + 1) % 26 + 'a';
 
-        if (currentChar == str2[j] || incrementedChar == str2[j]) {
+        if (currchar == str2[j] || inchar == str2[j]) {
             j++;
         }
     }
