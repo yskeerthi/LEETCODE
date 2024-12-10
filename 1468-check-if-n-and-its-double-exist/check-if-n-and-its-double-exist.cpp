c5ae4d425242e7f8e37028b678d6dfaa72b9fc03
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-12-10 21:23:05 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (97.63%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        int zeroCount = 0;
-        for (int num : arr) {
-            if (num == 0) {
-                zeroCount++;
-            }
-        }
-        if (zeroCount >= 2) {
-            return true;
-        }
-
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.size(); j++) {
-                if (i != j) {
-                    if (arr[j] == 2 * arr[i] || (arr[i] % 2 == 0 && arr[j] == arr[i] / 2)) {
-                        return true;
-                    }
+        int i;
+        for(i = 0; i < arr.size(); i++) {
+            for(int j = i + 1; j < arr.size(); j++) {
+                if(  i!=j && arr[j] == 2*arr[i] || arr[i]==2*arr[j]) {
+                    return true;
                 }
             }
         }
