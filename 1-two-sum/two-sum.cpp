05550a79c429cd4159b407f13e37b528b06a9929
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2024-12-17 16:36:49 +0530

    Time: 2 ms (82.49%) | Memory: 14.5 MB (28.66%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>vec;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                int sum=0;
-                sum+=nums[i]+nums[j];
-                 if(sum==target)
-                 {
-                    vec.push_back(i);
-                    vec.push_back(j);
-                 }
+        vector<int> original = nums; 
+        vector<int> result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; 
+            int low = i + 1, high = nums.size() - 1; 
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (nums[mid] == complement) {
+                    for (int j = 0; j < original.size(); j++) {
+                        if (original[j] == nums[i] || original[j] == complement) {
+                            result.push_back(j);
+                        }
+                    }
+                    return result;
+                } else if (nums[mid] < complement) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
-           
         }
-        return vec;
+        
+        return result; 
     }
 };
