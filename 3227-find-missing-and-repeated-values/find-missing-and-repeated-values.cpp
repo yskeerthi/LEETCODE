e85071430ac16f5a65b9cccc9b34c0e3491c706f
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2025-03-06 18:04:00 +0530

    Time: 3 ms (77.46%) | Memory: 26.8 MB (77.75%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int>v;
-        vector<int>res;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0; j<grid[i].size(); j++){
-                v.push_back(grid[i][j]);
+        int n = grid.size();
+        int size = n * n;
+        vector<int> count(size + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                count[grid[i][j]]++;
             }
         }
-        for(int j=0;j<v.size();j++)
-        {
-            int c=count(v.begin(),v.end(),v[j]);
-            if(c==2)
-            {
-                res.push_back(v[j]);
-                break;
+        int a=-1,b=-1;
+        for (int num = 1; num <= size; num++) {
+            if (count[num] == 2) {
+                a = num;
+            } else if (count[num] == 0) {
+                b = num;
             }
         }
-        sort(v.begin(),v.end());
-        for(int k=1;k<=v.size();k++)
-        {
-            int cnt= count(v.begin(),v.end(),k);
-            if(cnt==0){
-                res.push_back(k);
-                break;
-            }
-        }
-        
-        return res;
+
+        return {a, b};
     }
 };
