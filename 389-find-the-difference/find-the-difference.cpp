5bb61d235123849a2386f0d8e41c649c5fbf00e8
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2025-06-17 18:14:57 +0530

    Time: 5 ms (2.54%) | Memory: 9 MB (95.28%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        char res = 0;  // This will hold the extra character
-        for (int i = 0; i < t.size(); i++) {
-            res ^= t[i];  // XOR all characters in t
+   char findTheDifference(string s, string t) {
+    for (char ch : t) {
+        int countInS = count(s.begin(), s.end(), ch);
+        int countInT = count(t.begin(), t.end(), ch);
+        if (countInT > countInS) {
+            return ch;
         }
-        for (int i = 0; i < s.size(); i++) {
-            res ^= s[i];  // XOR all characters in s
-        }
-        return res;  // The result will be the extra character
     }
+    return ' ';
+}
+
+    
 };
